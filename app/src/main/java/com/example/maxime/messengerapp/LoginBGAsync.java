70eb169789f7bb0a2264e806ed35ea1096fd27ba
victor <victor@gerfaud.fr>
2016-10-19 21:14:19 +0200

    LoginIntotheApp

--- app/src/main/java/com/example/maxime/messengerapp/LoginBGAsync.java
+++ app/src/main/java/com/example/maxime/messengerapp/LoginBGAsync.java
@@ -1,7 +1,12 @@
 package com.example.maxime.messengerapp;
 
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
+
+import com.example.maxime.messengerapp.service.SignInService;
+import com.example.maxime.messengerapp.service.User;
 
 /**
  * Created by maxime on 12/10/16.
@@ -11,32 +16,37 @@ public class LoginBGAsync extends AsyncTask <String, Void, Boolean>  {
 
     private final String TAG = LoginBGAsync.class.getName();
 
-
+    private Activity myactivity = null;
     public LoginListener loginListener;
-    //private boolean result;
+
+    public LoginBGAsync(Activity myactivity) {
+        this.myactivity = myactivity;
+    }
 
     public void setLoginListener(LoginListener loginListener) {
         this.loginListener = loginListener;
     }
 
+
     @Override
-    protected void onPostExecute(Boolean result) {
-        Log.i(TAG,result.toString());
-        //loginListener.onLogin(result);
+    protected void onPreExecute() {
+        myactivity.findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
     }
 
     @Override
     protected Boolean doInBackground(String[] params) {
-        Log.i(TAG, "doInBackground \n params :  " + params[0].toString() );
-//        String login = params[0].toString();
-//        String pwd = params[1].toString();
-
-        //TODO : Check DB for User
-        return true;
+        String login = params[0].toString();
+        String pwd = params[1].toString();
+        User user = new User(login,pwd);
+        return SignInService.signInResponse(user);
     }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        myactivity.findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+    }
+
     public interface LoginListener{
         void onLogin(boolean result);
-
-
     }
 }
